fc167551270cc5b683d8a830a20cb4ee72620bd0
jwhitish <jacobwhitish@gmail.com>
2018-11-07 06:31:09 -0800

    building login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  #logs in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #returns the current logged-in user, if any
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
 end
